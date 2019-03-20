e3903a5c85f71d7779c732c9fd850c8f11407059
Serhii Subbotin <serezhazp@gmail.com>
2019-03-20 17:01:01 +0200

    refactor model

--- domain/src/main/java/com/serezhazp/twitterclient/domain/Tweet.kt
+++ domain/src/main/java/com/serezhazp/twitterclient/domain/Tweet.kt
@@ -3,9 +3,9 @@ package com.serezhazp.twitterclient.domain
 import java.io.Serializable
 
 data class Tweet(
-    var text: String,
+    val text: String,
     val createdAt: String = "",
     val user: TwitterUser,
-    var media: String? = null,
+    val media: String? = null,
     val hashTags: List<String>? = emptyList()
 ) : Serializable
